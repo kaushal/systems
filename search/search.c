95713dc9571c1ed1702cc545449a8c9d99fa09e5
Kaushal Parikh <kaushalp88@gmail.com>
2013-10-24 18:05:52 -0400

    linked list created, and added to data structure

--- search/search.c
+++ search/search.c
@@ -6,11 +6,22 @@
 #include <sys/stat.h>
 #include "uthash.h"
 
+typedef struct listNode {
+    struct listNode *next;
+    char *fileName;
+    int count;
+} listNode;
+
+struct wordHash {
+    struct listNode *head;
+    const char *word;
+    UT_hash_handle hh;
+};
+
 char *substring(char *string, int position, int length)
 {
     char *pointer;
     int c;
-
     pointer = malloc(length+1);
 
     if (pointer == NULL)
@@ -36,10 +47,14 @@ char *substring(char *string, int position, int length)
 int main(int argc, char *argv[]){
     FILE *fp;
     int i = 0, first = 0;
+    int tempOpen = 0, tempClose = 0;
+    char *word;
+    struct listNode *head = NULL, *node = NULL, *current;
+    struct wordHash *s, *tmp, *wordHashMap= NULL;
     fp = fopen(argv[1], "r");
 
     if(!fp){
-        printf("File not found");
+        printf("File not found\n");
         return 0;
     }
     char line[256];
@@ -48,14 +63,50 @@ int main(int argc, char *argv[]){
             line[strlen(line) -1] = '\0';
         }
         char buff[256];
+        head = NULL;
         first = 0;
         for(i = 0; i < strlen(line); i++){
             if(line[i] == ' ' && first == 0){
-                char *pointer = substring (line, 0, i);
-                printf("%s\n", pointer);
+                word = substring (line, 0, i);
+                printf("%s\n", word);
+                tempOpen = 0;
+                tempClose = 0;
+                for(; i < strlen(line); i++){
+                    if(line[i] == '('){
+                        tempOpen = i+2;
+                    }
+                    if(line[i] == ' ' && tempOpen != 0){
+                        tempClose = i + 1;
+                    }
+                    if(tempOpen != 0 && tempClose != 0 && head == NULL){//add to linked list
+                        listNode *temp = malloc(sizeof(listNode));
+                        temp->fileName = substring(line, tempOpen, tempClose - tempOpen);
+                        printf("first: %s\n", temp->fileName);
+                        head = temp;
+                        current = head;
+                        tempOpen = 0;
+                        tempClose = 0;
+
+                    }
+                    else if(tempOpen != 0 && tempClose != 0 && head != NULL){
+                        listNode *temp = malloc(sizeof(listNode));
+                        temp->fileName = substring(line, tempOpen, tempClose - tempOpen);
+                        printf("next: %s\n", temp->fileName);
+                        current->next = temp;
+                        current = temp;
+                        tempOpen = 0;
+                        tempClose = 0;
+                    }
+                }
+                s = (struct wordHash*)malloc(sizeof(struct wordHash));
+                s->word = word;
+                s->head = head;
+                HASH_ADD_KEYPTR(hh, wordHashMap, word, strlen(word), s);
                 first = 1;
-}
+                break;
+            }
         }
+
     }
     return 0;
 }
