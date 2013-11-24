dd9f47d35d3dcde3bb3c2a4dad4d03de5af364b1
Stewart Smith <stewartlsmith12@gmail.com>
2013-11-24 16:19:49 -0500

    failed experiment with categories

--- multithread/main.c
+++ multithread/main.c
@@ -4,30 +4,44 @@
 #include <stdlib.h>
 #include "uthash.h"
 
-struct customerInfo {
+struct customer {
     char * name;
-    int customerID;
-    double credit;
+    int customerID;         /* key */
+    double balance;
     char * address;
     char * state;
     char * areaCode;
-};
-
-struct customerHash {
-    int customerID;                   /* key */
-    struct customerInfo *customer;    /* value */
     UT_hash_handle hh;
 };
 
+struct sharedData {
+    int         isopen;
+    unsigned int    front;
+    unsigned int    count;
+    unsigned int    bufsize;
+    pthread_mutex_t mutex;
+    pthread_cond_t  dataAvailable;
+    pthread_cond_t  spaceAvailable;
+};
+
+struct bookOrder {
+    char * title;
+    double price;
+    int customerID;
+    char * category;
+};
+
 /*Global Hash Table*/
-struct customerHash *customersHashTable = NULL;
+struct customer *customersHashTable = NULL;
 
 /*Method Declarations*/
-struct customerInfo * makeCustomer(char *line);
-void addCustomer(struct customerInfo *cInfo);
+struct customer * makeCustomer(char *line);
+void addCustomer(struct customer *cInfo);
+char ** processCategories(char * categories);
 
 int main(int argc, char * argv[])
 {
+    /*Argument check */
     if(argc != 4) {
         printf("USAGE: The first argument is the name of the database input file\n");
         printf("The second is the name of the book order input file.\n");
@@ -36,7 +50,7 @@ int main(int argc, char * argv[])
         return 0;
     }
 
-    //Read and check files
+    /*Read and check files*/
     FILE *dbFile = fopen(argv[1], "r");
     FILE *bookFile = fopen(argv[2], "r");
     if( dbFile == NULL || bookFile == NULL) {
@@ -45,19 +59,23 @@ int main(int argc, char * argv[])
     }
 
     char line[256];
-
+    /*Create customers hash table*/
     while(fgets(line, sizeof(line), dbFile)) {
-       struct customerInfo *newCustomer =  makeCustomer(line);
+       struct customer *newCustomer =  makeCustomer(line);
        printf("newCustomer: %s\n", newCustomer->name);
        addCustomer(newCustomer);
     }
 
-    return 0;
+    /* Process Categories */
+    char * allCategories = argv[3];
+    char ** categoriesArray = processCategories(allCategories);
+
+    pthread_exit(0);
 }
 
-struct customerInfo * makeCustomer(char *line)
+struct customer * makeCustomer(char *line)
 {
-    struct customerInfo *c = malloc(sizeof(struct customerInfo));
+    struct customer *c = malloc(sizeof(struct customer));
 
     char * token = strtok(line, "|");
     c->name = token;
@@ -72,9 +90,28 @@ struct customerInfo * makeCustomer(char *line)
     return c;
 }
 
-void addCustomer(struct customerInfo *cInfo) {
-    struct customerHash *c;
-    c = malloc(sizeof(struct customerHash));
+void addCustomer(struct customer *cInfo)
+{
+    struct customer *c;
+    c = malloc(sizeof(struct customer));
     c->customerID = cInfo->customerID;
     HASH_ADD_INT(customersHashTable, customerID, c );  /* Arguments: Hash Table, key, value*/
+}
+
+char ** processCategories(char * categories)
+{
+    int categoryCount = 0;
+
+    char * token = strtok(categories, " ");
+    if(token) categoryCount++;
+    while((token = strtok(NULL, " ")) != NULL) {
+        categoryCount++;
+    }
+    char **categoriesArray[categoryCount];
+    token = strtok(categories, " ");
+    if(token) categoryCount++;
+    while((token = strtok(NULL, " ")) != NULL) {
+        printf("working\n");
+    }
+    return categoriesArray;
 }
